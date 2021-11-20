632c723d0880abd05f4a7729194431a9c3e30dec
Jared Nolen <Legodoghead@gmail.com>
2021-11-20 15:44:31 -0800

    Added basic position system. With 3 enemies

---
+++ 170TakingTurnsInTeams/Assets/Scripts/Position.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public GameObject character;
+    public bool occupied;
+    public PositionManager pm;
+
+    private void Start()
+    {
+        pm = GameObject.FindGameObjectWithTag("PositionManager").GetComponent<PositionManager>();
+    }
+
+    void Update()
+    {
+        // If a charecter is assinged to this postion change bool
+        if (character == null)
+            occupied = false;
+        else
+            occupied = true;
+    }
+
+    public void asignPosition(GameObject chara)
+    {
+        // Set charecter to this position
+        character = chara;
+        character.transform.position = this.transform.position;
+    }
+
+    public void unasignPosition()
+    {
+        // Set charecter to this position
+        character = null;
+    }
+
+    private void OnMouseDown()
+    {
+        // If the player has not selected a character
+        if (pm.selectedCharacter == null)
+        {
+            if (character == null)
+                Debug.Log("Nothing here!");
+            else
+            {
+                if (character.tag == "PlayerCharacter")
+                {
+                    Debug.Log("Clicked on: " + character.name);
+                    pm.selectedCharacter = character;
+                    pm.selectedCharacterlocation = this.gameObject;
+                }
+                else if (character.tag == "Enemy")
+                {
+                    Debug.Log("Clicked on: " + character.name);
+                    Debug.Log("Cannot select an enemy!");
+                }
+            }
+        }
+        // IF the player has selected a character
+        else
+        {
+            if (character == null)
+            {
+                Debug.Log("Moving " + pm.selectedCharacter.name + " to " + this.gameObject.name);
+                // Unasign old pos
+                pm.selectedCharacterlocation.GetComponent<Position>().unasignPosition();
+                // Asign new pos
+                asignPosition(pm.selectedCharacter);
+                pm.selectedCharacter = null;
+                pm.selectedCharacterlocation = null;
+            }
+            else
+            {
+                if (character.tag == "PlayerCharacter")
+                {
+                    Debug.Log("Clicked on: " + character.name);
+                    pm.selectedCharacter = character;
+                    pm.selectedCharacterlocation = this.gameObject;
+                }
+                else if (character.tag == "Enemy")
+                {
+                    Debug.Log("Cannot move to an enemy occupied space!");
+                }
+            }
+        }
+        
+    }
+}
---
+++ 170TakingTurnsInTeams/Assets/Scripts/PositionManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionManager : MonoBehaviour
+{
+    // Player Charecters
+    public GameObject rouge;
+    public GameObject mage;
+    public GameObject knight;
+
+    // Enemies
+    public GameObject goblin;
+
+    // Default Postions
+    public GameObject defaultTop;
+    public GameObject defaultMiddle;
+    public GameObject defaultBottom;
+
+    // Enemy positions
+    public Transform[] enemyPositions;
+
+    // Selected charecter
+    public GameObject selectedCharacter;
+    public GameObject selectedCharacterlocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Instantiate enemies.
+        for (int i = 0; i < enemyPositions.Length; i++)
+        {
+            GameObject enemy = Instantiate(goblin, enemyPositions[i].position, enemyPositions[i].rotation);
+            enemyPositions[i].GetComponent<Position>().character = enemy;
+        }
+
+        // Set PC to default positions
+        defaultTop.GetComponent<Position>().asignPosition(rouge);
+        defaultMiddle.GetComponent<Position>().asignPosition(mage);
+        defaultBottom.GetComponent<Position>().asignPosition(knight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
