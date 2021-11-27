a277cd7a39670054d87078dc9baff6d4d152af94
Stryker Buffington <sbuffing@ucsc.edu>
2021-11-27 14:24:05 -0800

    Modified Attack Class to have cardinal damage multipliers

--- 170TakingTurnsInTeams/Assets/Scripts/Action scripts/Attack.cs
+++ 170TakingTurnsInTeams/Assets/Scripts/Action scripts/Attack.cs
@@ -17,6 +17,15 @@ public class Attack : ScriptableObject  //This really should be called attack ba
     [SerializeField]
     int manaPoints;
 
+    [Header("A value of -1 will hide that move in that location")]
+    [Header("N,E,S,W refers to enemy location relative to player")]
+    [Header("Cardinal Damage Multiplier: [Neutral, North, East, South, West]")]
+    
+    
+
+    [SerializeField]
+    float[] cardinalDamageMultiplier;
+
     public string NameOfAttack{
         get { return nameOfAttack; }
     }
